beecfae35f749133f691128fe154d6317fb400ee
VikingoUrbano <cristian.g.rico@gmail.com>
2022-10-17 21:45:43 -0300

    interfaz terminada, agregando funcionalidades a vetapp

--- VetApp/barra-lateral/script.js
+++ VetApp/barra-lateral/script.js
@@ -1,8 +1,15 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const navigation = document.querySelector('.navigation');
+const mainframe = document.querySelector('.mainframe');
 
 menuToggle.onclick = () => {
   navigation.classList.toggle('open');
+  mainframe.classList.toggle('open');
+}
+
+function cerrarNav() {
+  navigation.classList.remove('open');
+  mainframe.classList.remove('open');
 }
 
 const listItems = document.querySelectorAll('.list-item');
--- VetApp/js/busqueda.js
+++ VetApp/js/busqueda.js
@@ -1,13 +1,28 @@
+var mostrarDATA = document.querySelector(".mostrarDatos");
+
 
 function filtrar(){
+    ocultar();
     console.log("aca se haria la consulta de datos en la DB para llenar el listado");
 }
 
 
 // aca estaria la funcion que salta a la web de los datos de la consulta seleccionada con los valores que traiga de la DB 
 
-function seleccionar(){
-    let Home = this.location
-    Home.href = "../formularios/nvaCons.html";
+// function seleccionar(){
+//     let Home = this.location
+//     Home.href = "../formularios/nvaCons.html";
+    
+// }
+
+
+function mostrar() {
+    mostrarDATA.classList.remove("mostrarDatos");
+    mostrarDATA.scrollIntoView({behavior: "smooth"}); 
+
+}
+
+function ocultar() {
+    mostrarDATA.classList.add("mostrarDatos");
     
 }
